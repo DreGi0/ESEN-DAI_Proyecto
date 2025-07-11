8ee6c73e0b4b62019cc50669db751c95be6cec97
Tiffany Meléndez <tiffany.melendez01@gmail.com>
2025-07-11 05:41:12 -0600

    Add files via upload

---
+++ inventory_controller.py
@@ -0,0 +1,38 @@
+from Model import inventory_model
+from datetime import datetime
+
+class InventoryController:
+    def __init__(self):
+        self.movements = []
+
+    def get_all_inventory(self):
+        return inventory_model.get_all_inventory()
+
+    def get_all_products(self):
+        return inventory_model.get_all_products()
+
+    def add_inventory_movement(self, id_prod, nombre_prod, cantidad, tipo_movimiento):
+        self.movements.append({
+            'id_prod': id_prod,
+            'nombre_prod': nombre_prod,
+            'cantidad': cantidad,
+            'tipo_movimiento': tipo_movimiento
+        })
+
+    def get_current_stock(self, id_prod):
+        result = inventory_model.get_stock_by_product(id_prod)
+        return result[0] if result else 0
+
+    def save_movements(self):
+        fecha = datetime.now().strftime("%Y-%m-%d")
+        for item in self.movements:
+            inventory_model.insert_inventory_movement(
+                item['id_prod'],
+                item['cantidad'],
+                fecha,
+                item['tipo_movimiento']
+            )
+        return True
+
+    def reset_movements(self):
+        self.movements = []
